350f782b6177605786b4fd58c6bd833d93ee1460
s5062849 <killercrabs11@virginmedia.com>
2018-10-19 14:53:48 +0100

    Added collectibles and annimaiton

---
+++ Assets/Scripts/Collectables.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectables : MonoBehaviour {
+
+	private AudioSource source; //the players audio source
+	[SerializeField] private AudioClip clip; //The audio that will play
+
+
+	void OnTriggerEnter (Collider col){
+		
+
+		Debug.Log ("Just hit: " + gameObject.name);
+
+
+		source = col.GetComponent<AudioSource>();
+		source.PlayOneShot (clip, 1.0f);
+
+
+		Destroy (gameObject);
+
+	}
+
+
+
+}
---
+++ Assets/Scripts/Places.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Places : MonoBehaviour {
+
+	private AudioSource source; //the players audio source
+	[SerializeField] private AudioClip clip; //The audio that will play
+
+
+	void OnTriggerEnter (Collider col){
+
+
+		Debug.Log ("You have visited: " + gameObject.name);
+
+
+		source = col.GetComponent<AudioSource>();
+		source.PlayOneShot (clip, 1.0f);
+
+
+		Destroy (gameObject);
+
+	}
+}
