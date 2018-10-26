de9b3d138aec2bde3f47c079b099c10d6026b1df
s5062849 <killercrabs11@virginmedia.com>
2018-10-26 15:09:35 +0100

    Added a file save and file load methods with collectibles scripts

--- Assets/Scripts/Collectables.cs
+++ Assets/Scripts/Collectables.cs
@@ -7,18 +7,33 @@ public class Collectables : MonoBehaviour {
 	private AudioSource source; //the players audio source
 	[SerializeField] private AudioClip clip; //The audio that will play
 
+	CollectiblesController cc; //creates an instance of CollectiblesController
+
+	void Start()
+	{
+		//gets the CollectiblesController's game object
+		GameObject ccgo = GameObject.Find ("CollectiblesController");
+		//makes the instance of cc equal to the script CollectiblesController
+		cc = ccgo.GetComponent<CollectiblesController> ();
+	}
+
 
 	void OnTriggerEnter (Collider col){
 		
-
+		//outputs the game object that you have hit
 		Debug.Log ("Just hit: " + gameObject.name);
 
-
+		//gets the audio source
 		source = col.GetComponent<AudioSource>();
+		//plays the audio source
 		source.PlayOneShot (clip, 1.0f);
 
-
+		//destroys the game engine
 		Destroy (gameObject);
+
+		//calls the function Incrementcount and passes the game object that we collided with
+		cc.Incrementcount (gameObject);
+
 
 	}
 
---
+++ Assets/Scripts/CollectiblesController.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+public class CollectiblesController : MonoBehaviour {
+
+	public CollectiblesData[] cd;
+
+
+	void Awake()
+	{
+		//this means that data is kept between scenes
+		DontDestroyOnLoad (gameObject);
+
+		//this means that it does not creat more than one type of this object and will not create errors
+		if (FindObjectsOfType (GetType ()).Length > 1) 
+		{
+			Destroy (gameObject);
+		}
+
+
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown ("s")) 
+		{
+			SaveData ();
+		}
+		if (Input.GetKeyDown ("l")) 
+		{
+			LoadData ();
+		}
+	}
+
+
+	public void Incrementcount(GameObject go)
+	{
+		if (go.name.Contains ("Heart")) 
+		{
+			cd [0].collectibleNum++;
+		}
+		else if (go.name.Contains ("Star")) 
+		{
+			cd [1].collectibleNum++;
+		}
+		else if (go.name.Contains ("Diamond")) 
+		{
+			cd [2].collectibleNum++;
+		}
+
+
+		OutputCounts ();
+
+	}
+
+
+	private void OutputCounts()
+	{
+		Debug.Log ("Youve collected:");
+		Debug.Log("Hearts: " + cd [0].collectibleNum);
+		Debug.Log ("Stars: " + cd [1].collectibleNum);
+		Debug.Log ("Diamonds: " + cd [2].collectibleNum);
+	}
+
+
+	public void SaveData()
+	{
+		BinaryFormatter bf = new BinaryFormatter ();
+		FileStream fs = File.Create (Application.persistentDataPath + "/gameData.dat");
+		bf.Serialize (fs, cd);
+		fs.Close ();
+		Debug.Log ("Data Saved.");
+	}
+
+
+	public void LoadData()
+	{
+		if (File.Exists (Application.persistentDataPath + "/gameData.dat")) {
+			BinaryFormatter bf = new BinaryFormatter ();
+			FileStream fs = File.Open (Application.persistentDataPath + "/gameData.dat", FileMode.Open);
+			cd = (CollectiblesData[])bf.Deserialize (fs);
+			fs.Close ();
+			Debug.Log ("Data Loaded.");
+		}
+		else 
+		{
+			Debug.LogError ("The file you are trying to load does not exists");
+		}
+	}
+
+
+
+}
