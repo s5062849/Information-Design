b84e2ad2702a5a8a08e2fec4a4b98399ad432296
Michael Smith <killercrabs11@virginmedia.com>
2018-11-19 23:04:32 +0000

    pulled from repository

---
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour 
+{
+
+	private const float minAngleY = 0.0f;
+	private const float maxAngleY = 50.0f;
+
+	public Transform lookAt;
+	public Transform camTransform;
+
+	private Camera cam;
+
+	private float distance = 5.0f;
+	private float currentX = 0.0f;
+	private float currentY = 0.0f;
+
+	private void Start()
+	{
+		camTransform = transform;
+		cam = Camera.main;
+
+	}
+
+	private void Update()
+	{
+		currentX += Input.GetAxis ("Mouse X");
+		currentY += Input.GetAxis ("Mouse Y");
+
+		currentY = Mathf.Clamp (currentY, minAngleY, maxAngleY);
+
+	}
+
+
+	private void LateUpdate()
+	{
+		Vector3 dir = new Vector3 (0, 0, -distance);
+		Quaternion rotation = Quaternion.Euler (currentY, currentX, 0);
+		camTransform.position = lookAt.position + rotation * dir;
+		camTransform.LookAt (lookAt.position);
+	}
+
+}
